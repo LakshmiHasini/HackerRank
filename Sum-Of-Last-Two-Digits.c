d65ebc2adc60dd2270dcf8fab0fb81f76a84c408
Batchu Lakshmi Hasini <lakshmihasini8@gmail.com>
2025-11-21 09:21:55 +0530

    Add program to sum last digits of two integers

---
+++ Sum-Of-Last-Two-Digits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int sumLastDigits(int a, int b)
+{
+    int sum=a%10+b%10;
+    return sum;
+}
+int main() {
+
+ int a,b;
+    scanf("%d %d",&a,&b);
+    int res= sumLastDigits(a,b);
+    printf("The sum of last digits is: %d",res);
+    return 0;
+}
