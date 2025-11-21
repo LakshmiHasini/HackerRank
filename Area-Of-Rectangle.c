667d09fe2e366a9c9792f881d6299ce206caae9e
Batchu Lakshmi Hasini <lakshmihasini8@gmail.com>
2025-11-21 09:19:44 +0530

    Add Area-Of-Rectangle.c to calculate area

---
+++ Area-Of-Rectangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int area(int length, int width)
+{
+    int res=length*width;
+    return res;
+}
+int main() {
+    int length, width;
+    scanf("%d %d",&length,&width);
+    int total= area(length,width);
+    printf("The area is: %d units",total);
+    return 0;
+}
