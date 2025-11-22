a60813165092881505d56c70bf26b8c67cf335fb
Batchu Lakshmi Hasini <lakshmihasini8@gmail.com>
2025-11-22 13:23:39 +0530

    Add Half-The-Number program

---
+++ Half-The-Number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+   int a;
+    scanf("%d",&a);
+    float res=a/2.00;
+    printf("Half of %d is %.2f",a,res);
+    
+    return 0;
+}
