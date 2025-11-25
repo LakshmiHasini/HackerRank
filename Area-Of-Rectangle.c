4e955ca7bac2b09fe403a99a40434dba3cf540bb
Batchu Lakshmi Hasini <lakshmihasini8@gmail.com>
2025-11-25 23:09:09 +0530

    Implement area calculation for rectangle
    Added area function to calculate rectangle area.

--- Area-Of-Rectangle.c
+++ Area-Of-Rectangle.c
@@ -2,11 +2,13 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+
 int area(int length, int width)
 {
     int res=length*width;
     return res;
 }
+
 int main() {
     int length, width;
     scanf("%d %d",&length,&width);
