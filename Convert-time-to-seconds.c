25d67a683eb225b60b2a9016e7c380f14e0f8a66
Batchu Lakshmi Hasini <lakshmihasini8@gmail.com>
2025-12-01 22:52:13 +0530

    Fix calculation of total seconds in toSeconds function

--- Convert-time-to-seconds.c
+++ Convert-time-to-seconds.c
@@ -5,13 +5,17 @@
 
 int toSeconds(int hours, int minutes, int seconds)
 {
+    
     int total= hours*60*60+minutes*60+seconds;
     return total;
+    
 }
 int main() {
+    
     int hrs,min,sec;
     scanf("%d %d %d",&hrs,&min,&sec);
     printf("Total seconds: %d",toSeconds(hrs,min,sec));
        
     return 0;
+    
 }
